896b8f05da3e38dfc99820a289d7ab78fbb2b7dc
gautsaur <46569499+gautsaur@users.noreply.github.com>
2020-07-08 18:37:19 -0600

    Updated scrolling for project head

--- scripts/moveBackground.js
+++ scripts/moveBackground.js
@@ -1,25 +1,30 @@
 
-    var up=0;
-    var down=0;
-
-$(document).ready(function() {
-    $("#projectScroll").on("scroll", function(e) {
+$(document).ready(function () {
+    $("#projectScroll").on("scroll", function (e) {
         horizontal = e.currentTarget.scrollLeft;
         if (horizontal >= 51) {
             $('#projectHead').addClass(" activeScroll");
+
+            $('.left-paddle').fadeIn();
         } else {
             $('#projectHead').removeClass(" activeScroll");
+            $('.left-paddle').fadeOut()
+        }
+        var maxScroll = $('#projectScroll').get(0).scrollWidth - $('#projectScroll').get(0).clientWidth;
 
+        if (horizontal >= maxScroll) {
+            $('.right-paddle').fadeOut();
+        } else {
+            $('.right-paddle').fadeIn();
         }
     });
-
+    paddles();
     disableButtons();
     clickBulk();
-
 });
 
 function clickBulk() {
-    $("#nav a, #name a, #backToTop a").on('click', function(event) {
+    $("#nav a, #name a, #backToTop a").on('click', function (event) {
         // Make sure this.hash has a value before overriding default behavior
 
         // Figure out element to scroll to
@@ -38,4 +43,24 @@ function disableButtons() {
         var btns = inactivePages[i].querySelectorAll('button');
         btns.disabled = true;
     }
+}
+
+function paddles() {
+    $('.right-paddle')
+        .on({
+            mouseenter: function () {
+                $('#projectScroll').animate({ scrollLeft: "+=2000" }, 4000)
+            },
+            mouseleave: function () {
+                $('#projectScroll').stop();
+            },
+        });
+    $('.left-paddle').on({
+        mouseenter: function () {
+            $('#projectScroll').animate({ scrollLeft: "-=3000" }, 4000)
+        },
+        mouseleave: function () {
+            $('#projectScroll').stop();
+        }
+    });
 }
